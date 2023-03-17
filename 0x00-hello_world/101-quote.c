b59e5674f9d6f377e5c593319dc3a6438ade8cf2
Veeviyan <snowygeenie@gmail.com>
2023-03-17 05:39:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - prints out the lat part of a quote in the standard error
- *
+ * main - prints out the last part of a quote in the standard error
  * Return: return 1 if success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n", 58);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
