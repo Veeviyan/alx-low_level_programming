5684a16f121e3355e410968246c3f070175e2e24
Veeviyan <snowygeenie@gmail.com>
2023-03-24 11:14:54 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - function to be checked
+ *
+ * @n: parameter to be checked
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int n;
+
+	for (n = 95; n <= 57; n++)
+	{
+		if (n = 95 && n <= 57)
+		{
+			_putchar("n\n");
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}
