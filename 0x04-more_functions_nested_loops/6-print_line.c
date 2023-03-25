966d223ff89122af6536efb1aca8753eaeb0b0bf
Veeviyan <snowygeenie@gmail.com>
2023-03-25 10:01:14 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,18 +10,18 @@
 
 void print_line(int n)
 {
-        int m;
+	int m;
 
-        for (m = 0; m < n ; m++)
-        {
-                if (n == 0)
-                {
-                        _putchar('\n');
-                }
-                else
-                {
-                        _putchar('_');
-                }
-        }
-        _putchar('\n');
+	for (m = 0; m < n ; m++)
+	{
+		if (n == 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
 }
