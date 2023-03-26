8e5932d869b6711cb6e68a40352d992141a6c91d
Veeviyan <snowygeenie@gmail.com>
2023-03-26 14:10:17 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to be checked
+ *
+ * @n: parameter to be checked
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int m;
+
+	if (n == 0)
+	{
+		for (m = 0; m <= n; m++)
+		{
+			_putchar('\');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * print_square - function to be checked
+ * @size: parameter to be used
+ * Return: void
+ */
+
+void print_square(int size);
