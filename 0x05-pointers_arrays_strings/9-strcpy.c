070ab4230277a61a660dc0522a554474702b2858
Veeviyan <snowygeenie@gmail.com>
2023-03-29 07:43:51 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - pointer function
+ * @dest: first pointer
+ * @src: second pointer
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
