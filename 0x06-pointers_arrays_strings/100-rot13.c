b4365a487dbba188f69a6e91662c076729f04381
Veeviyan <snowygeenie@gmail.com>
2023-03-30 15:52:44 -0700

    101-print-number.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,21 +1,33 @@
 #include "main.h"
-
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * rot13 - function to be used
  */
-void print_number(int n)
+
+char *rot13(const char *src)
 {
-	unsigned int num = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+	if(src == NULL){
+      return NULL;
+    }
+  
+    char* result = malloc(strlen(src));
+    
+    if(result != NULL){
+      strcpy(result, src);
+      char* current_char = result;
+      
+      while(*current_char != '\0'){
+        //Only increment alphabet characters
+        if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90)){
+          if(*current_char > 109 || (*current_char > 77 && *current_char < 91)){
+            //Characters that wrap around to the start of the alphabet
+            *current_char -= 13;
+          }else{
+            //Characters that can be safely incremented
+            *current_char += 13;
+          }
+        }
+        current_char++;
+      }
+    }
+    return result;
 }
