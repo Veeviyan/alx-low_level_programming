0c1d576e2227e2b64a2c609221da6ed18750c49c
Veeviyan <snowygeenie@gmail.com>
2023-03-30 06:43:19 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - function to be used
+ * @dest: first pointer
+ * @src: second pointer
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int destl = 0;
+	int srcl = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destl++;
+
+	for (i = 0; src[i] != '\0'; i++)
+		srcl++;
+	for (i = 0; i <= dest[i]; i++)
+		dest[destl + i] = src[i];
+	return (dest);
+}
