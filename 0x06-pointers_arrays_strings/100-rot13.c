de95e5152f57b9b708eb461284adb1dbcf0b4042
Veeviyan <snowygeenie@gmail.com>
2023-03-30 16:30:17 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,33 +1,28 @@
 #include "main.h"
 /**
  * rot13 - function to be used
+ * @s: pointer to string
+ * Return: *s
  */
 
-char *rot13(const char *src)
+char *rot13(char *s)
 {
-	if(src == NULL){
-      return NULL;
-    }
-  
-    char* result = malloc(strlen(src));
-    
-    if(result != NULL){
-      strcpy(result, src);
-      char* current_char = result;
-      
-      while(*current_char != '\0'){
-        //Only increment alphabet characters
-        if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90)){
-          if(*current_char > 109 || (*current_char > 77 && *current_char < 91)){
-            //Characters that wrap around to the start of the alphabet
-            *current_char -= 13;
-          }else{
-            //Characters that can be safely incremented
-            *current_char += 13;
-          }
-        }
-        current_char++;
-      }
-    }
-    return result;
+	int i;
+	int j;
+	char data1[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] =
+"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] == datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
