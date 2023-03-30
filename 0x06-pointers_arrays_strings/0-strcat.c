c992adf55a22d4eac73ced47ac0a6cea06867b94
Veeviyan <snowygeenie@gmail.com>
2023-03-30 06:50:02 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,11 +14,18 @@ char *_strcat(char *dest, char *src)
 	int srcl = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
+	{
 		destl++;
-
+	}
 	for (i = 0; src[i] != '\0'; i++)
+	{
 		srcl++;
+	}
 	for (i = 0; i <= dest[i]; i++)
+	{
 		dest[destl + i] = src[i];
+	}
 	return (dest);
+
+	_putchar('\n');
 }
