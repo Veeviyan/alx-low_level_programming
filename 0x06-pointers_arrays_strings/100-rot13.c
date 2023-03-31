92297f97245019edbf0e9410060053f319db503e
Veeviyan <snowygeenie@gmail.com>
2023-03-31 07:14:53 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,21 +7,17 @@
 
 char *rot13(char *s)
 {
-	int i;
-	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int n = 1;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		if (s[i] += 'a'-'z')
 		{
-			if (s[i] == data1[j])
-			{
-				s[i] == datarot[j];
-				break;
-			}
+			s[i];
 		}
 	}
+
+	
 	return (s);
 }
