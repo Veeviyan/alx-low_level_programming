6cdb26b4d7d606de707da843a0edf525676a251f
Veeviyan <snowygeenie@gmail.com>
2023-04-10 11:12:06 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if two arguments are passed and 1 2 argumentsare not passed
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int mul;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+	printf("%d\n", mul);
+
+	return (0);
+}
