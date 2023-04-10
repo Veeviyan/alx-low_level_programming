a4e073884ff44d779d402e3bf02d1ab128f8a70a
Veeviyan <snowygeenie@gmail.com>
2023-04-10 09:07:09 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
-#include <stdlib.h>
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - function to use
+ * @argc: argument count
+ * @argv: argumrnt vector
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i = atoi(argv[0]);
-
-	if (argc >= 1)
-	for (i = 0; i < argc; i++)
-		printf("%d\n", i);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
