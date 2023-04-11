8fe2730f8412c39150be68c0ebbacd771cd02a40
Veeviyan <snowygeenie@gmail.com>
2023-04-11 11:45:32 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,8 +17,9 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		str[i] = c;
+	else
+		for (i = 0; i < size; i++)
+			str[i] = c;
 
 	return (str);
 }
