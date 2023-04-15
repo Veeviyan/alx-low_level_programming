0748b7c0bbb9c90e45e12a34a2a115dfd984a0c3
Veeviyan <snowygeenie@gmail.com>
2023-04-15 10:14:31 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,6 +5,7 @@
  * free_grid - functioin to use
  * @grid: pointer to the array
  * @height: parameter
+ * Return: 0
  */
 
 void free_grid(int **grid, int height)
@@ -12,7 +13,9 @@ void free_grid(int **grid, int height)
 	int i;
 
 	for (i = 0; i < height; i++)
+	{
 		free(grid[i]);
+	}
 
 	free(grid);
 }
