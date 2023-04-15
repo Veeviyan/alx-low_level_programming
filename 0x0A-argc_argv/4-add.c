157bf050499332818eb1b56190f43e87db7ac554
Veeviyan <snowygeenie@gmail.com>
2023-04-15 09:28:26 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,25 +1,44 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv)
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
 {
+	char *s;
 	int i;
+	unsigned int j;
+	unsigned int add = 0;
 
-	if (argc == 1)
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			s = argv[i];
+			for (j = 0; j < strlen(s); j++)
+			{
+				if (s[j] < 48 || s[j] > 57)
+				{
+					printf("Error\n");
+				}
+			}
+			add = add + atoi(s);
+			s++;
+		}
+		printf("%d\n", add);
+	}
+	else
 	{
 		printf("0\n");
 	}
-	if (argc )
-	{
-		printf("Error\n");
-		return (1);
-	}
-	
-	i = atoi(argv[0]);
-
-	for (i = 0; i < argc; i++)
-		printf("%d\n", i);
 
 	return (0);
 }
