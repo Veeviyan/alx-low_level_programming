7732db783659704fb6422189363806927664164a
Veeviyan <snowygeenie@gmail.com>
2023-04-17 12:46:25 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - user-defined data type
+ * @name: user name
+ * @age: user age
+ * @owner: user owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
