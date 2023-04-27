26af702c3c76a0dbfac4ae0c3a3a18c0aa2af4ec
Veeviyan <snowygeenie@gmail.com>
2023-04-27 11:59:59 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+  * list_len - prints the length ofa list
+  * @h: singly linked lists
+  * Return: number of nodes
+  */
 size_t list_len(const list_t *h)
 {
 	size_t nodes;
