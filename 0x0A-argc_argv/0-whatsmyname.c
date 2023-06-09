51012805eeb66d3b4b524ea7e56ca8738e593462
Vivian <snowygeenie@gmail.com>
2023-06-09 13:51:12 -0700

    solution to task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - function to check code
+ * @argc: number of arguments passed to the main function
+ * @argv: number of command line arguments
+ * Return: 0
+ **/
+
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
