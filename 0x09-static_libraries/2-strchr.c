5219f16927109e71870c2582739f290cda10fe81
Vivian <snowygeenie@gmail.com>
2023-06-09 12:57:44 -0700

    I created a static library

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates character in a string
+ * @s: a pointer to the string
+ * @c: character to be searched for
+ * Return: index if matched, -1 if not.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+
+	return (0);
+}
