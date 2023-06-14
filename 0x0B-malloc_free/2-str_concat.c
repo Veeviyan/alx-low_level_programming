96d7daf954d0d901865e6b2b808d139e515ed9e1
Vivian <snowygeenie@gmail.com>
2023-06-14 06:01:56 -0700

    Updated solution to task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,20 +10,18 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i, j, k = 0;
-	int len = 0;
+	int len1, len2 = 0;
 	char *twostr;
 
-	if (s1 == NULL)
-		s1 = "";
+	if (s1 != NULL)
+		for (i = 0; s1[i]; i++)
+			len1++;
 
-	if (s2 == NULL)
-		s2 = "";
+	if (s2 != NULL)
+		for (j  = 0; s2[j]; j++)
+			len2++;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		for (j = 0; s2[j] != '\0'; j++)
-			len++;
-
-	twostr = malloc(len * sizeof(char));
+	twostr = malloc((i + j + 1) * sizeof(char));
 
 	if (twostr == NULL)
 		return (NULL);
