25af716f136f54d980a8f1ec2463a6b511feed2a
Vivian <snowygeenie@gmail.com>
2023-06-16 02:43:25 -0700

    Updated Solution to task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,19 +14,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *old_ptr;
 	unsigned int i;
 
+	if (new_size == old_size)
+                return (ptr);
+
+	if (new_size == 0 && ptr != NULL)
+        {
+                free(ptr);
+                return (NULL);
+        }
 	if (ptr == NULL)
 	{
 		return (malloc(new_size));
 	}
 	new_ptr = malloc(new_size);
-
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (new_size == old_size)
-		return (ptr);
 
 	if (new_ptr == NULL)
 		return (NULL);
