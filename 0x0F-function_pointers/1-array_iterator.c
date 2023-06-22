c3e549c1047f4cf5d0dcb5d03725e440a1ede5ad
Vivian <snowygeenie@gmail.com>
2023-06-21 22:04:07 -0700

    solution to task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,6 +5,7 @@
 
 /**
  * array_iterator - prints an array
+ * @array: array of elements
  * @size: size of the array
  * @action: function pointer
  * Return: 0
@@ -15,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array != NULL && action != NULL)
 	{
-		for( i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
