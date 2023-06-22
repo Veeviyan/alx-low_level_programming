3dd565a5bd213fcc57a988b32952c603e0d0182c
Vivian <snowygeenie@gmail.com>
2023-06-22 04:12:11 -0700

    Udated solution to task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list numbers;
 	unsigned int i;
-	unsigned int digits;
 
 	va_start(numbers, n);
 
@@ -21,8 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		digits = va_arg(numbers, unsigned int);
-		printf("%d", digits);
+		printf("%d", va_arg(numbers, int));
 		if (i < (n - 1))
 			printf("%s", separator);
 	}
