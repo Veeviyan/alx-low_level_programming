c596957e2ab94f8f0e78b20d61066a1fef376191
Vivian <snowygeenie@gmail.com>
2023-07-05 04:48:06 -0700

    Solution to task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * print_listint - prints all the elements in a listint
+ * @h: the first element in the list
+ * Return: number of nodes in the linked list
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
