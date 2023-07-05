0931a0d3fdec98689c924f07fe22be987ab1fc35
Vivian <snowygeenie@gmail.com>
2023-07-05 06:17:32 -0700

    Solution to task 5

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: first node of the list
+ * Return: vnothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+}
