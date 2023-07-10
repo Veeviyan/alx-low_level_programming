b3eff2c4fc668e38b8cb8b35caf636142dd7cb0f
Veeviyan <snowygeenie@gmail.com>
2023-07-10 10:52:56 -0700

    Updated Solution to task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,42 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: file to create
- * @text_cotent: content to write into the file
- * Return: 1 on success and -1 on failure
- */ 
+  * create_file - creates a file
+  * @filename: file to be created
+  * @text_content: strimg to be written
+  *
+  * Return: 1 on success and -1 on failure
+  */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int i = 0;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (i = 0; text_content[i] != '\0'; i++)
-		{
-			write(file, text_content, i);
-		}
-	}
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
 
-	close(file);
-
+	close(fd);
 	return (1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
